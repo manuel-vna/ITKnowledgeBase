30a5cf7b2b12f69318a5cb5bef77a1a76d06eca9
Manuel.von.Au <vonau.manuel@gmail.com>
2022-03-07 22:44:35 +0100

    add, ImportExportFragment.java, import file picker: not finished, still buggy

--- app/src/main/java/com/example/itkbproject/ImportExportFragment.java
+++ app/src/main/java/com/example/itkbproject/ImportExportFragment.java
@@ -3,12 +3,16 @@ package com.example.itkbproject;
 
 import static java.lang.Math.toIntExact;
 import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,6 +20,11 @@ import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
@@ -51,6 +60,13 @@ public class ImportExportFragment extends Fragment {
     private TextView ViewProgressImport;
     private Double ImportProgressThreshold;
     private Integer androidVersion;
+
+    public static final int PICKFILE_RESULT_CODE = 1;
+    private TextView tvItemPath;
+    private Uri uri;
+    private String filePath;
+
+
 
     @Nullable
     @Override
@@ -173,34 +189,119 @@ public class ImportExportFragment extends Fragment {
 
 
     private void importDb() {
+
+        ActivityResultLauncher<Intent> sActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            Uri uri = data.getData();
+                            File file= new File(uri.getPath());
+
+                            Log.d("Debug_A", String.valueOf(file.isFile()));
+
+
+
+                            List<List<String>> lines = new ArrayList<>();
+
+                            inputFileSize = file.length();
+                            Log.d("Debug_A", "inputFileSize: "+String.valueOf(inputFileSize));
+
+                            progressBarImport.setMax(toIntExact(inputFileSize));
+
+
+                            ExecutorService executor = Executors.newSingleThreadExecutor();
+                            executor.submit(new Runnable() {
+                                public void run() {
+
+
+                                    Cursor cursor = appDb.entryDao().getAllEntriesasCursor();
+                                    cursor.moveToLast();
+                                    LastDbId = cursor.getCount();
+                                    //Log.d("Debug_A", "Database Last Line: "+String.valueOf(cursor.getCount()));
+
+                                    try{
+                                        inputStream = new Scanner(file);
+
+                                        while(inputStream.hasNext()){
+                                            String line= inputStream.next();
+                                            String[] values = line.split(";");
+                                            lines.add(Arrays.asList(values));
+
+
+                                            //progressBar
+                                            progressStatusImport += line.getBytes().length; //toIntExact(line.getBytes().length);
+                                            progressBarImport.setProgress(toIntExact(line.getBytes().length));
+                                            ViewProgressImport.setText(progressStatusImport+"/"+progressBarImport.getMax()+" Bytes");
+                                            ImportProgressThreshold = inputFileSize-((inputFileSize/100.0)*10); // Invisible-Threshold: 90% of max
+                                            if (progressStatusImport >= ImportProgressThreshold){
+                                                binding.ImportExportProgressBarImport.setVisibility(View.INVISIBLE);
+                                                binding.ImportExportProgressTextViewImport.setVisibility(View.INVISIBLE);
+                                            }
+
+                                            LastDbId += 1;
+
+                                            if (Arrays.asList(values).size() < 5){
+                                                Log.d("Debug_A", "Not enough values in line "+Arrays.asList(values).get(0));
+                                                continue;
+                                            }
+
+
+                                            Entry entry = new Entry(LastDbId,
+                                                    Arrays.asList(values).get(0),
+                                                    Arrays.asList(values).get(1),
+                                                    null,
+                                                    Arrays.asList(values).get(2),
+                                                    Arrays.asList(values).get(3),
+                                                    Arrays.asList(values).get(4));
+
+                                            appDb.entryDao().insertEntry(entry);
+                                            //Log.d("Debug_A", String.valueOf(entry));
+
+                                            try {
+                                                Thread.sleep(2100);
+                                            } catch (InterruptedException e) {
+                                                e.printStackTrace();
+                                            }
+
+
+                                        }
+
+                                        inputStream.close();
+                                    }
+                                    catch (FileNotFoundException e) {
+                                        e.printStackTrace();
+                                        Log.d("Debug_A", String.valueOf(e));
+
+                                    }
+                                }
+                            });
+
+                        }
+                    }
+                });
+
+
         binding.ImportExportButtonImport.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
+                Intent data = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                data.setType("*/*");
+                data = Intent.createChooser(data, "Choose a file");
+                sActivityResultLauncher.launch(data);
+
+            }
+        });
 
 
-                if (androidVersion  <= 29) {
-
-                    if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
-
-                        // Requesting the permission
-                        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 101);
-                    } else {
-                        Toast.makeText(getContext(), "Permission 'Read Storage' granted", Toast.LENGTH_SHORT).show();
-                    }
-                }
-
-                else {
-                    if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_MEDIA_LOCATION) == PackageManager.PERMISSION_DENIED) {
-
-                        //ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_MEDIA_LOCATION}, 101);
-                        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_MEDIA_LOCATION}, 101);
-
-                    } else {
-                        Toast.makeText(getContext(), "Permission 'Read Storage' granted", Toast.LENGTH_SHORT).show();
-                    }
-                }
+    }
 
 
+
+                 /*
 
                 binding.ImportExportProgressBarImport.setVisibility(View.VISIBLE);
                 binding.ImportExportProgressTextViewImport.setVisibility(View.VISIBLE);
@@ -208,10 +309,13 @@ public class ImportExportFragment extends Fragment {
                 progressBarImport = binding.ImportExportProgressBarImport;
                 ViewProgressImport = binding.ImportExportProgressTextViewImport;
 
-                String pathname = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
-                pathname += "/db-export3.csv";
+                //String pathname = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
+                //pathname += "/db-export3.csv";
 
-                File file= new File(pathname);
+                //File file= new File(pathname);
+                File file= new File(uri.getPath());
+
+
                 List<List<String>> lines = new ArrayList<>();
 
                 inputFileSize = file.length();
@@ -289,6 +393,10 @@ public class ImportExportFragment extends Fragment {
             }
         });
     }
+
+
+ */
+
 
 
 
